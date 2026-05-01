b94d25f992ed783e93887fd615e997077f7360b4
rafa.santos <rafael@magazineluiza.com.br>
2026-05-01 07:04:00 -0300

    6.12: Using Flyway to clean the test database

--- src/test/java/com/algaworks/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/customer/query/CustomerQueryServiceIT.java
@@ -17,8 +17,8 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Sql(scripts = "classpath:sql/clean-database.sql",  executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-//@Sql(scripts = "classpath:sql/clean-database.sql",  executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+//@Sql(scripts = "classpath:db/clean/afterMigrate.sql",  executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//@Sql(scripts = "classpath:db/clean/afterMigrate.sql",  executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @Transactional
 class CustomerQueryServiceIT {
     @Autowired
